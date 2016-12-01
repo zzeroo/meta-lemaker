e38a1850887649a8925a51b01e3aedcdb18aa4e3
Stefan Müller <s.mueller@it.kls-glt.de>
2016-12-01 12:57:33 +0100

    Downgrade to linux kernel 4.6, dysfunctional

--- recipes-bsp/bpro-u-boot-scr/files/bananapro/boot.cmd
+++ recipes-bsp/bpro-u-boot-scr/files/bananapro/boot.cmd
@@ -1,5 +1,6 @@
-bootdelay=0
-setenv bootargs console=ttyS0,115200 root=/dev/mmcblk0p2 rootwait panic=10 vt.global_cursor_default=0 quiet splash
-load mmc 0:1 0x43000000 ${fdtfile} || load mmc 0:1 0x43000000 boot/${fdtfile}
-load mmc 0:1 0x42000000 uImage || load mmc 0:1 0x42000000 boot/uImage
-bootm 0x42000000 - 0x43000000
+setenv fdtfile sun7i-a20-bananapro.dtb
+setenv bootargs 'earlyprintk console=ttyS0,115200 root=/dev/mmcblk0p2 rootfstype=ext4 rootwait noinitrd'
+mmc dev 0
+fatload mmc 0:1 ${kernel_addr_r} uImage
+fatload mmc 0:1 ${fdt_addr_r} ${fdtfile}
+bootm ${kernel_addr_r} - ${fdt_addr_r}
---
+++ recipes-kernel/linux/linux-yocto_4.6.bbappend
@@ -0,0 +1,3 @@
+FILESEXTRAPATHS_prepend := "${THISDIR}/${PN}:"
+SRC_URI += "file://defconfig"
+COMPATIBLE_MACHINE_lemaker = "bananapro"
